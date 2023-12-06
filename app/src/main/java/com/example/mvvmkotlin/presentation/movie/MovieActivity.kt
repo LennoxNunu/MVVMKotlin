2e6b4ee44654837ffda6de3b7e543aa257222b98
LennoxNunu <lennoxnunu@gmail.com>
2023-12-06 15:16:03 +0200

    presentation layer part four

--- app/src/main/java/com/example/mvvmkotlin/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/example/mvvmkotlin/presentation/movie/MovieActivity.kt
@@ -3,9 +3,12 @@ package com.example.mvvmkotlin.presentation.movie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmkotlin.R
 import com.example.mvvmkotlin.databinding.ActivityMovieBinding
 import com.example.mvvmkotlin.presentation.di.Injector
@@ -16,6 +19,7 @@ class MovieActivity : AppCompatActivity() {
     lateinit var factory: MovieViewModelFactory
     private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
+    private lateinit var adapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_movie)
@@ -23,10 +27,32 @@ class MovieActivity : AppCompatActivity() {
             .inject(this)
         movieViewModel= ViewModelProvider(this,factory)
             .get(MovieViewModel::class.java)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        Log.i("MOVTAG","movie activity init recycler view")
+        binding.movieRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter()
+        binding.movieRecyclerView.adapter = adapter
+        displayPopularMovies()
+    }
+
+    private fun displayPopularMovies(){
+        Log.i("MOVTAG","movie activity display popular artist")
+        binding.movieProgressBar.visibility = View.VISIBLE
         val responseLiveData = movieViewModel.getMovies()
         responseLiveData.observe(this, Observer {
-            Log.i("MYTAG",it.toString())
+            if(it!=null){
+                Log.i("MOVTAG","observed ${it.toString()}")
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
         })
-
     }
+
 }
